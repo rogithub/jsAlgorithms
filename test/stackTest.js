1a349d2a846a1f8e20329eb3cf448c6047b76fab
rodrigo debian <correo.rodrigo@gmail.com>
2019-05-20 13:11:38 -0500

    more testing

--- test/stackTest.js
+++ test/stackTest.js
@@ -5,6 +5,38 @@ describe("Stack", function(){
     describe("isEmpty", function() {
 	it("Should be true when created", function(){
 	    assert.ok(new Stack().isEmpty());
-	})
+	});
+    });
+
+    describe("peek", function() {
+	it("Should return the last added element", function(){
+	    let stack = new Stack();
+	    stack.push(5);
+	    stack.push(8);
+
+	    let pick = stack.peek();
+	    assert.equal(pick, 8);
+
+	    assert.equal(2, stack.size());
+	    assert.equal(false, stack.isEmpty());
+	    
+	});
+    });
+
+    describe("pop", function(){
+	it("Should remove last added element", function(){
+
+	    let stack = new Stack();
+	    stack.push(5);
+	    stack.push(8);
+	    stack.push(11);
+	    assert.equal(3, stack.size());
+
+	    let eleven = stack.pop();
+	    assert.equal(11, eleven);
+
+	    let eight = stack.pop();
+	    assert.equal(8, eight);
+	});
     });
 });
