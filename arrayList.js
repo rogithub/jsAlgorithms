444cda22d095ebbd6deea980e7090a221d71db74
rodrigo debian <correo.rodrigo@gmail.com>
2019-05-23 16:17:59 -0500

    one more test

---
+++ arrayList.js
@@ -0,0 +1,26 @@
+let ArrayList = function() {
+    let array = [];
+    this.insert = function (element){
+	array.push(element);
+    }
+
+    this.bubbleSort = function () {
+	let length = array.lenght;
+	for (let i = 0; i < length; i++) {
+	    for (let j = 0; j < length - 1; i++) {
+		if (array[j] > array[j+1]) {
+		    swap(array, j, j+1);
+		}
+	    }
+	}
+    }
+    
+    this.toString = function() {
+	return array.join();
+    }
+};
+
+
+module.exports = ArrayList;
+
+
---
+++ test/sortTest.js
@@ -0,0 +1,11 @@
+let assert = require ("assert");
+let ArrayList = require("./../arrayList");
+
+describe("ArrayList", function(){
+    describe("isEmpty", function() {
+	it("Should be true when created", function(){
+	    let arr = new ArrayList();
+	    assert.ok(true);
+	});
+    });
+});
