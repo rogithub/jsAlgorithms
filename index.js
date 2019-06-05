bdd178b2eaa97bf81be9b335dc287a14cfc8e946
rodrigo debian <correo.rodrigo@gmail.com>
2019-06-05 16:46:06 -0500

    adding fariness

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-let lcg = require("./algo/linearCongruentialGen.js");
-let gen = lcg(7, 5, 11);
+let lcg = require("./algo/fairLinearCongruentialGen.js");
+let gen = lcg(7, 5, 11, 50, 100);
 
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
 console.log(gen.next().value);
 console.log(gen.next().value);
 console.log(gen.next().value);
