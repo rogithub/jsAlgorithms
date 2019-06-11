7173c38596c30836eaa80071c3fcfe829149c600
Rodrigo Debian <rosec@protonmail.ch>
2019-06-10 20:13:06 -0500

    add pow

--- algo/pow.js
+++ algo/pow.js
@@ -5,19 +5,18 @@ module.exports = (a, b) => {
     let square = x => x * x;
     let sum = a;
     let i = 2;
-    let counter = 0;
+    let counter = 1;
 
     if (b === 1) return a;
 
-    for (i = 2; i < b; i*=2) {	
-	sum = square(sum);
-	counter +=1;
+    for (i = 2; i <= b; i*=2) {
+	counter*=2;
+	sum = square(sum);	
     }
     
-    console.log("counter:", counter);
-    if (counter < b) {
-	console.log("add one");
-	return sum * a;
+    while (counter < b) {
+	counter++;
+	sum = sum  * a;
     }
 
   
