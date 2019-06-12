c1a36d67625c2af8917f53512ac52b3e7e004df2
rodrigo debian <correo.rodrigo@gmail.com>
2019-06-12 13:17:15 -0500

    renaming file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let heap = require("./algo/heap.js");
-let heapToArr = require("./algo/heapToArrays.js");
+let treeToArr = require("./algo/treeToArrays.js");
 let ptree = require("./algo/printTree.js");
 
 let arr = [1,2,3,4,5,6,7,8,9,10,11,12];
 let arrH = heap(arr);
-let lists = heapToArr(arrH);
+let lists = treeToArr(arrH);
 console.log(arrH)
 console.log(ptree( lists ));
