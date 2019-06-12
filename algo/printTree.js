173fbdcf85ad8348be3f3af90d07ffe6eed4054a
rodrigo debian <correo.rodrigo@gmail.com>
2019-06-12 12:58:59 -0500

    improve aligment

--- algo/printTree.js
+++ algo/printTree.js
@@ -3,17 +3,19 @@
 module.exports = (lists) => {
     let l = `\n`;
     let text = '';
-    let print = (i) => i === undefined ? '-' : i.toString();
     let spaces = (i) => Array(i).join(" ");
-    let max = 64;
-    
-    
+
+    let pad = lists[0][0].toString().length;
+    let max = lists[lists.length - 1].length;
+    let print = (i) => i === undefined ? '*'.padStart(pad) : i.toString().padStart(pad);
+    max = max * 2; // 2 width ratio ( it can be increased by 2 multiples 2, 4, 8, etc.)
+
     for (let i = 0; i < lists.length; i++) {
 	for (let j = 0; j < lists[i].length; j++) {
 	    text += spaces(max) + print(lists[i][j]) + spaces(max);
 	}
 	max = max / 2;
-	text += `${l}`
+	text += l;
     }
 
     return text;
