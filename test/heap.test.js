a34b1fe0cf255cbdbc96d471094d3cf019f9aebc
rodrigo debian <correo.rodrigo@gmail.com>
2021-05-07 14:21:59 -0500

    refactor

--- test/heap.test.js
+++ test/heap.test.js
@@ -42,20 +42,4 @@ describe('Heap', () => {
             } while (arr.length > 1)
         });
     });
-
-    describe('speed', () => {
-        it('priority queue, sorted', () => {
-            let arr = [2, 5, 10, 12, 6, 8, 1, 3, 7, 9, 4, 11];
-            do {
-                let heapified = build(arr);
-                let max = arr.shift(heapified);
-
-                let maxAll = arr.reduce(function (a, b) {
-                    return Math.max(a, b);
-                });
-
-                expect(max).to.be.greaterThanOrEqual(maxAll);
-            } while (arr.length > 1)
-        });
-    });
 });
