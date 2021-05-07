9cb522314cc287ed37623de4dce2088eef7c44ab
rodrigo debian <correo.rodrigo@gmail.com>
2021-05-07 14:21:11 -0500

    refactor

--- treePrinter/index.js
+++ treePrinter/index.js
@@ -1,4 +1,22 @@
-let treeToArrays = require('./treeToArrays');
+treeToArrays = (arr) => {
+
+    let lists = [];
+
+    let i = 0;
+    let size = 1;
+    while (i < arr.length) {
+        let list = [];
+        for (let y = 0; y < size; y++) {
+            list.push(arr[i]);
+            i++;
+        }
+        lists.push(list);
+        size *= 2;
+    }
+
+    return lists;
+}
+
 
 module.exports = (arr) => {
     let lists = treeToArrays(arr);
