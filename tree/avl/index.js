2e4ca09b730f133e83af669584459854e2c3e37a
rodrigo debian <correo.rodrigo@gmail.com>
2021-05-10 13:54:22 -0500

    fix

--- tree/avl/index.js
+++ tree/avl/index.js
@@ -1,14 +1,28 @@
-
+let { traverseBackward, traverseForward, inArray } = require('../index');
 let tree = [];
 
+let rotateLeft = (arr, node) => {
+
+}
+
+let rotateRight = (arr, node) => {
+
+}
+
 let add = (item) => {
-    if (tree.length === 0) {
-        tree.push(item);
-        return;
-    }
+    tree.push(item);
+
+    if (tree.length === 1) return;
+
+    traverseBackward(tree, ({ i, parent, left, right }) => {
+        if (i === left && tree[parent] >= tree[i]) return;
+        if (i === right && tree[parent] >= tree[i]) return;
+
+    });
+
 }
 
 
 module.exports = {
-    getHeight
+    add
 }
